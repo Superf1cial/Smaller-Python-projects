762f99e1a4ed6497cca594bc11d06b923b79fc43
Superf1cial <overwatchanalyst@gmail.com>
2016-09-21 09:47:59 +0200

    Update Graphic.py

--- Graphic.py
+++ Graphic.py
@@ -1,37 +1,46 @@
 import matplotlib.pyplot as plt
-import csv
+import matplotlib.animation as animation
 import matplotlib.dates as dates
+import csv
 import datetime as dt
 
-x = []
-y = []
-x2 = []
-y2 = []
+
 
 fig = plt.figure()
 ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
 
-with open('example1.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        print(row)
-        x.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
-        y.append(row[1])
+def animate(i):
+    x = []
+    y = []
+    x2 = []
+    y2 = []
 
-with open('example2.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        print(row)
-        x2.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
-        y2.append(row[1])
+    with open('example1.csv','r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if len(row) > 1:
+                x.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
+                y.append(row[1])
+    ax2.clear()
+    ax1.plot(x,y)
 
-ax1.plot(x,y)
-ax2.plot(x2,y2)
+    with open('example2.csv','r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if len(row) > 1:
+                print(row)
+                x2.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
+                y2.append(row[1])
+    ax2.clear()
+    ax2.plot(x2,y2)
+
 ax1.set_xlabel("Timestamp")
 ax1.set_ylabel("Temperature")
-ax2.set_xlabel("Timestmap")
+ax2.set_xlabel("Timestamp")
 ax2.set_ylabel("Humidity")
+
+ani = animation.FuncAnimation(fig, animate, interval= 60000)
 
 plt.title('Interesting Graph')
 plt.legend()
