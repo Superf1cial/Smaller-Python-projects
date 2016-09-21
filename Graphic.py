3ba6a0e0d898ca8551699eb8c4c30d5d07dae450
Superf1cial <overwatchanalyst@gmail.com>
2016-09-21 09:07:16 +0200

    Update Graphic.py

--- Graphic.py
+++ Graphic.py
@@ -1,25 +1,38 @@
 import matplotlib.pyplot as plt
 import csv
+import matplotlib.dates as dates
+import datetime as dt
 
 x = []
 y = []
+x2 = []
+y2 = []
 
 fig = plt.figure()
 ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
 
-with open('example.csv','r') as csvfile:
+with open('example1.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        print(row[0])
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+        print(row)
+        x.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
+        y.append(row[1])
+
+with open('example2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        print(row)
+        x2.append(dt.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
+        y2.append(row[1])
 
 ax1.plot(x,y)
+ax2.plot(x2,y2)
+ax1.set_xlabel("Timestamp")
+ax1.set_ylabel("Temperature")
+ax2.set_xlabel("Timestmap")
+ax2.set_ylabel("Humidity")
 
-
-plt.xlabel('x')
-plt.ylabel('y')
 plt.title('Interesting Graph')
 plt.legend()
 plt.show()
